106514b26dcf887286ed2788df4bc77f3fbdea12
richiedevr <richie.rosario@protonmail.com>
2023-02-25 20:47:11 -0600

    books promise added for books.json

--- src/services.js
+++ src/services.js
@@ -2,5 +2,36 @@ export const fetchWithTimeout = (delay) => {
   return new Promise( resolve => setTimeout(resolve,delay))
 }
 
+export const fetchMovies = () => {
+  return fetch("./data/movies.json")
+    .then(response => response.json())
+    .then(movies => movies)
+    .catch(error => console.log(error));
+}
+
+export const fetchBooks = () => {
+  return fetch("./data/books.json")
+    .then(response => response.json())
+    .then(books => books)
+    .catch(error => console.log(error));
+}
+
+export const getBooksOrMovies = () => {
+  return promise.all([fetchBooks(), fetchMovies()])
+    .then(results => {
+      books,
+        movies
+    })
+  .catch(error => console.log("Error fetching books and movies",error))
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies()
+
+getBooksAndMoviesPromise.then(results => {
+  console.log('getBooksOrMoviesPromise',results)
+})
+
+
+
 
 
