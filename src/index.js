8c47c37d8d7a8a0522d1549e906c2282f9329793
richiedevr <richie.rosario@protonmail.com>
2023-02-26 00:35:20 -0600

    fixed logging of promise results

--- src/index.js
+++ src/index.js
@@ -1,32 +1,29 @@
 import { fetchWithTimeout, fetchMovies, fetchBooks } from './services';
-
-// const movies = require('./data/movies.json');
+const movies = require('./data/movies.json');
 
 const getBooksAndMovies = () => {
   return Promise.all([fetchBooks(), fetchMovies()])
     .then(([books, movies]) => {
       books,
       movies
-
-    })
+})
     .catch(error => console.log("Error fetching books and movies,", error));
 }
 
 const getBooksAndMoviesPromise = getBooksAndMovies();
-
-getBooksAndMoviesPromise.then((results) => {
+getBooksAndMoviesPromise.then(results => {
   console.log('getBooksAndMoviesPromise', results);
-
-})
+});
 
 function getBooksOrMovies() {
   return Promise.race([fetchBooks(), fetchMovies()])
     .then(results => results)
-    .catch(error => console.log('Error waiting for the promise race', error));
+    .catch(error => {
+      console.log('Error waiting for the promise race', error);
+    });
 }
 
 const getBooksOrMoviesPromise = getBooksOrMovies();
-
-getBooksOrMoviesPromise.then((results) => {
+getBooksOrMoviesPromise.then(results => {
   console.log('getBooksOrMoviesPromise', results);
 })
